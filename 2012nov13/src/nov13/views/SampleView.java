2e78ef384cbcbc5c5cf16e1af3db81c9e1398efa
Jaxsun McCarthy Huggan <jaxsunm@gmail.com>
2012-11-16 14:14:47 -0800

    Basic command history implementation for REPL
    This is mostly a guess at how I would expect such a history to behave
    without doing any research
    
    * records commands as they are submitted
    * skips recording commands if they were selected directly from the
    history
    * resets history index when a command is modified

--- 2012nov13/src/nov13/views/SampleView.java
+++ 2012nov13/src/nov13/views/SampleView.java
@@ -3,6 +3,8 @@ package nov13.views;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import nov13.DebuggerMagic;
 
@@ -10,9 +12,14 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.console.IOConsole;
 import org.eclipse.ui.console.IOConsoleInputStream;
@@ -23,6 +30,8 @@ import org.eclipse.ui.part.ViewPart;
 public class SampleView extends ViewPart {
 
 	public static final String ID = "nov13.views.SampleView";
+	
+	private List<String> inputs = new ArrayList<String>();
 
 	private IOConsoleViewer viewer;
 
@@ -32,19 +41,56 @@ public class SampleView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		final IOConsole console = new IOConsole("REPL", null);
 		viewer = new IOConsoleViewer(parent, console);
-
+		
+		final IOConsoleOutputStream out = console.newOutputStream();
+		final IOConsoleInputStream in = console.getInputStream();
+		
+		// hate
+		final int suggestionIdx[] = {0};
+		final boolean shouldRecordHistory[] = {true};
+		
+		viewer.getTextWidget().addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				String character = ""+e.character;
+				if (e.keyCode == SWT.ARROW_UP && !inputs.isEmpty()) {
+					try {
+						IDocument document = viewer.getDocument();
+						int lineIndex = document.getNumberOfLines() - 1;
+						int index = suggestionIdx[0] > 0 ? suggestionIdx[0]-- : 0;
+						document.replace(document.getLineOffset(lineIndex), document.getLineLength(lineIndex), inputs.get(index));
+						putCaretAtBottom(document);
+						
+						shouldRecordHistory[0] = false;
+					} catch (BadLocationException e1) {
+						e1.printStackTrace();
+					}
+				}
+				// could be improved, eg deletions should also enter here
+				else if (character.matches("[\\w\\s]") && e.keyCode != SWT.CR && e.keyCode != SWT.KEYPAD_CR) {
+					shouldRecordHistory[0] = true;
+					suggestionIdx[0] = inputs.size() - 1;
+				}
+				super.keyPressed(e);
+			}
+		});
+		
 		Job job = new Job("REPL") {
 
-			IOConsoleOutputStream out = console.newOutputStream();
-			IOConsoleInputStream in = console.getInputStream();
-
 			DebuggerMagic magic = new DebuggerMagic();
+			
 
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				try {
 					String line = new BufferedReader(new InputStreamReader(in)).readLine();
 					magic.evaluate(line, out);
+					
+					if (shouldRecordHistory[0]) {
+						inputs.add(line);
+					}
+					suggestionIdx[0] = inputs.size() - 1;
+					
 					updateCaretPositionAfterPrint();
 					this.schedule();
 				} catch (IOException e) {
@@ -62,7 +108,7 @@ public class SampleView extends ViewPart {
 			@Override
 			public void documentChanged(DocumentEvent event) {
 				IDocument doc = event.getDocument();
-				viewer.setSelectedRange(doc.getLength(), 0);
+				putCaretAtBottom(doc);
 				doc.removeDocumentListener(this);
 			}
 
@@ -70,6 +116,10 @@ public class SampleView extends ViewPart {
 			public void documentAboutToBeChanged(DocumentEvent event) {
 			}
 		});
+	}
+	
+	private void putCaretAtBottom(IDocument doc) {
+		viewer.setSelectedRange(doc.getLength(), 0);
 	}
 
 	public void setFocus() {
