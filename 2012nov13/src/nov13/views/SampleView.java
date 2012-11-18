0a4f2611e9c88feb873e21062dc47a35e245544e
Adrian Kuhn <akuhn@iam.unibe.ch>
2012-11-17 21:28:05 -0800

    History works like bash history. Had to replace ListIterator with my own iterator as it returns the same element on alternating calls to next() and previous(), wtf!?

--- 2012nov13/src/nov13/views/SampleView.java
+++ 2012nov13/src/nov13/views/SampleView.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 import nov13.BullshitFree;
 import nov13.DebuggerMagic;
@@ -19,8 +18,8 @@ import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.console.IOConsole;
 import org.eclipse.ui.console.IOConsoleInputStream;
@@ -32,48 +31,14 @@ public class SampleView extends ViewPart {
 
 	public static final String ID = "nov13.views.SampleView";
 
-	private List<String> history = new ArrayList<String>();
-	private ListIterator<String> cursor = history.listIterator();
-
 	private IOConsoleViewer viewer;
 
-	public SampleView() {
-	}
-
-	int suggestionIdx = 0;
+	private History history = new History();
 
 	public void createPartControl(Composite parent) {
 		final IOConsole console = new IOConsole("REPL", null);
 		viewer = new IOConsoleViewer(parent, console);
-
-		viewer.getTextWidget().addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				try {
-					IDocument doc;
-					switch (e.keyCode) {
-					case SWT.ARROW_UP:
-						doc = viewer.getDocument();
-						if (cursor.hasNext()) {
-							int last = doc.getNumberOfLines() - 1;
-							doc.replace(doc.getLineOffset(last), doc.getLineLength(last), cursor.next());
-						}
-						putCaretAtBottom(doc);
-						break;
-					case SWT.ARROW_DOWN:
-						doc = viewer.getDocument();
-						if (cursor.hasPrevious()) {
-							int last = doc.getNumberOfLines() - 1;
-							doc.replace(doc.getLineOffset(last), doc.getLineLength(last), cursor.previous());
-						}
-						putCaretAtBottom(doc);
-						break;
-					}
-				} catch (BadLocationException exception) {
-					throw new BullshitFree(exception);
-				}
-			}
-		});
+		viewer.getTextWidget().addKeyListener(history);
 
 		Job job = new Job("REPL") {
 
@@ -86,8 +51,7 @@ public class SampleView extends ViewPart {
 				try {
 					String line = new BufferedReader(new InputStreamReader(in)).readLine();
 					magic.evaluate(line, out);
-					history.add(0, line);
-					cursor = history.listIterator();
+					history.add(line);
 					updateCaretPositionAfterPrint();
 					this.schedule();
 				} catch (IOException e) {
@@ -121,6 +85,48 @@ public class SampleView extends ViewPart {
 
 	public void setFocus() {
 		viewer.getControl().setFocus();
+	}
+
+	private class History implements KeyListener {
+
+		private List<String> list = new ArrayList();
+		private int index = 0;
+
+		public void add(String line) {
+			list.add(line);
+			index = list.size();
+		}
+
+		public String previous() {
+			if (index > 0) index--;
+			return list.get(index);
+		}
+
+		public String next() {
+			if (index < list.size()) index++;
+			if (index == list.size()) return "";
+			return list.get(index);
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.keyCode == SWT.ARROW_UP || e.keyCode == SWT.ARROW_DOWN) {
+				try {
+					IDocument doc = viewer.getDocument();
+					int last = doc.getNumberOfLines() - 1;
+					String line = e.keyCode == SWT.ARROW_UP ? history.previous() : history.next();
+					doc.replace(doc.getLineOffset(last), doc.getLineLength(last), line);
+					putCaretAtBottom(doc);
+				} catch (BadLocationException exception) {
+					throw new BullshitFree(exception);
+				}
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+
 	}
 
 }
