eb4613e114fd2081530bcc336d8d21e1c2d0913b
Adrian Kuhn <akuhn@iam.unibe.ch>
2012-11-22 23:37:41 -0800

    Wait until suspended before evaluating code. Uses a count down latch to make sure that the break point in our custom main method is hit before we attempt to evaluate code. It's not true that Java doesn't have beautiful APIs, it's just that java util concurrent is the only one.

--- my.eclipse.repl/src/my/eclipse/repl/DebuggerMagic.java
+++ my.eclipse.repl/src/my/eclipse/repl/DebuggerMagic.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IProject;
@@ -23,14 +25,17 @@ import org.eclipse.jdt.debug.core.IJavaMethodBreakpoint;
 import org.eclipse.jdt.debug.core.IJavaStackFrame;
 import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.core.IJavaValue;
-import org.eclipse.jdt.debug.core.JDIDebugModel;
 import org.eclipse.jdt.debug.eval.IEvaluationListener;
 import org.eclipse.jdt.debug.eval.IEvaluationResult;
+import org.eclipse.jdt.internal.debug.core.breakpoints.JavaMethodBreakpoint;
+import org.eclipse.jdt.internal.debug.core.model.JDIThread;
 import org.eclipse.jdt.internal.debug.ui.JavaDetailFormattersManager;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMRunner;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
+
+import com.sun.jdi.event.Event;
 
 /**
  * Runs example main method in example project, stops at method entry and
@@ -40,6 +45,7 @@ import org.eclipse.jdt.launching.VMRunnerConfiguration;
 public class DebuggerMagic {
 
 	private static final String MAIN_CLASS_NAME = "my.eclipse.repl.Main";
+
 	private IJavaProject myJavaProject;
 	private Launch launch;
 	private MyEvaluationEngine eval;
@@ -58,32 +64,24 @@ public class DebuggerMagic {
 
 		IVMInstall vmInstall = JavaRuntime.getVMInstall(myJavaProject);
 		if (vmInstall == null) vmInstall = JavaRuntime.getDefaultVMInstall();
-		if (vmInstall != null) {
-			IVMRunner vmRunner = vmInstall.getVMRunner(ILaunchManager.DEBUG_MODE);
-			if (vmRunner != null) {
-				String[] classPath = computeCustomClassPath();
-				if (classPath != null) {
-					VMRunnerConfiguration config = new VMRunnerConfiguration(MAIN_CLASS_NAME, classPath);
-					launch = new Launch(null, ILaunchManager.DEBUG_MODE, null);
+		if (vmInstall == null) return;
+		IVMRunner vmRunner = vmInstall.getVMRunner(ILaunchManager.DEBUG_MODE);
+		if (vmRunner == null) return;
+		String[] classPath = computeCustomClassPath();
+		VMRunnerConfiguration config = new VMRunnerConfiguration(MAIN_CLASS_NAME, classPath);
+		launch = new Launch(null, ILaunchManager.DEBUG_MODE, null);
 
-					IJavaMethodBreakpoint bp = createMagicBreakpoint();
-					vmRunner.run(config, launch, null);
+		IJavaMethodBreakpoint bp = createMagicBreakpoint();
+		vmRunner.run(config, launch, null);
 
-					// XXX Apparently we have to rely on the fact that things
-					// are slow and set the breakpoint after the configuration
-					// has been launched, the ScrapebookLauncher does seem to do
-					// it the same way.
+		// XXX Apparently we have to rely on the fact that things
+		// are slow and set the breakpoint after the configuration
+		// has been launched, the ScrapebookLauncher actually makes
+		// the same bold bet.
 
-					launch.getDebugTarget().breakpointAdded(bp);
-					// ((JDIDebugTarget)
-					// launch.getDebugTarget()).getEventDispatcher().addJDIEventListener(listener,
-					// request)
-
-					IJavaDebugTarget target = (IJavaDebugTarget) launch.getDebugTarget();
-					eval = new MyEvaluationEngine(myJavaProject, target);
-				}
-			}
-		}
+		launch.getDebugTarget().breakpointAdded(bp);
+		IJavaDebugTarget target = (IJavaDebugTarget) launch.getDebugTarget();
+		eval = new MyEvaluationEngine(myJavaProject, target);
 	}
 
 	private String[] computeCustomClassPath() {
@@ -118,9 +116,23 @@ public class DebuggerMagic {
 		throw new Error("No java project found!");
 	}
 
+	CountDownLatch isSuspended = new CountDownLatch(1);
+
 	private IJavaMethodBreakpoint createMagicBreakpoint() throws CoreException {
-		IJavaMethodBreakpoint bp = JDIDebugModel.createMethodBreakpoint(ResourcesPlugin.getWorkspace().getRoot(),
-				MAIN_CLASS_NAME, "main", "([Ljava/lang/String;)V", true, false, false, -1, -1, -1, 1, false, null);
+		IJavaMethodBreakpoint bp = new JavaMethodBreakpoint( //
+				ResourcesPlugin.getWorkspace().getRoot(), //
+				MAIN_CLASS_NAME, "main", "([Ljava/lang/String;)V", //
+				true, false, false, -1, -1, -1, 1, false, //
+				new HashMap()) {
+			@Override
+			public boolean handleBreakpointEvent(Event event, JDIThread thread, boolean suspendVote) {
+				try {
+					return super.handleBreakpointEvent(event, thread, suspendVote);
+				} finally {
+					isSuspended.countDown();
+				}
+			}
+		};
 		bp.setPersisted(false);
 		return bp;
 	}
@@ -146,14 +158,8 @@ public class DebuggerMagic {
 			return;
 		}
 		IThread[] threads = launch.getDebugTarget().getThreads();
-		// XXX Assuming that the last frame is suspended on our breakpoint.
+		isSuspended.await();
 		IJavaStackFrame frame = (IJavaStackFrame) threads[threads.length - 1].getTopStackFrame();
-		// XXX wait for breakpoint
-		// TODO we should use a listener to do this!
-		if (frame == null) {
-			Thread.sleep(500);
-			frame = (IJavaStackFrame) threads[threads.length - 1].getTopStackFrame();
-		}
 		IEvaluationListener callback = new IEvaluationListener() {
 			@Override
 			public void evaluationComplete(final IEvaluationResult result) {
