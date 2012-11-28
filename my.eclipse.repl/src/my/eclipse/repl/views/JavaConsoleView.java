77bfc2066030e7e83e2b85f1b376ef89bf523283
Adrian Kuhn <akuhn@iam.unibe.ch>
2012-11-28 11:44:29 -0800

    Auto completion on tab.

--- my.eclipse.repl/src/my/eclipse/repl/views/JavaConsoleView.java
+++ my.eclipse.repl/src/my/eclipse/repl/views/JavaConsoleView.java
@@ -3,12 +3,20 @@ package my.eclipse.repl.views;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.IHandler;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.ui.text.JavaTextTools;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerActivation;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
 
 public class JavaConsoleView extends ViewPart {
 
@@ -16,6 +24,10 @@ public class JavaConsoleView extends ViewPart {
 
 	private ConsoleViewer viewer;
 	private ReadEvaluatePrintLoop repl;
+
+	private IHandlerService service;
+
+	private IHandlerActivation activation;
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -35,6 +47,7 @@ public class JavaConsoleView extends ViewPart {
 
 	@Override
 	public void dispose() {
+		deactiveHandler();
 		repl.dispose();
 		super.dispose();
 	}
@@ -42,9 +55,25 @@ public class JavaConsoleView extends ViewPart {
 	private void configureSourceViewer(ISourceViewer viewer) {
 		Document doc = new Document();
 		viewer.setDocument(doc);
-
 		JavaTextTools tools = JDIDebugUIPlugin.getDefault().getJavaTextTools();
 		tools.setupJavaDocumentPartitioner(doc);
+		activateHandler();
+	}
+
+	private void activateHandler() {
+		IHandler handler = new AbstractHandler() {
+			public Object execute(ExecutionEvent event) throws org.eclipse.core.commands.ExecutionException {
+				viewer.doOperation(ISourceViewer.CONTENTASSIST_PROPOSALS);
+				return null;
+			}
+		};
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		service = (IHandlerService) workbench.getAdapter(IHandlerService.class);
+		activation = service.activateHandler(ITextEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS, handler);
+	}
+
+	private void deactiveHandler() {
+		service.deactivateHandler(activation);
 	}
 
 }
